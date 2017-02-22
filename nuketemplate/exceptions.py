a0e1955e4c280e9e106b392eddb6033fc09fa7a8
Florian Einfalt <florian.einfalt@me.com>
2017-02-22 21:04:02 +0000

    Add exception docstrings

--- nuketemplate/exceptions.py
+++ nuketemplate/exceptions.py
@@ -1,8 +1,16 @@
 class AbstractTemplateError(ValueError):
+    """
+    Exception indicating an error in the templating process,
+    inherits from :class:`ValueError`.
+    """
     def __init__(self, message):
         super(AbstractTemplateError, self).__init__(message)
 
 
 class AbstractGraphError(ValueError):
+    """
+    Exception indicating an error in the graph building process,
+    inherits from :class:`ValueError`.
+    """
     def __init__(self, message):
         super(AbstractGraphError, self).__init__(message)

160b1a43b2a8ae6060a0ae17c37f2f9f565a3af2
Florian Einfalt <florian.einfalt@me.com>
2017-02-24 19:50:18 +0000

    Update test runner

--- run_tests.py
+++ run_tests.py
@@ -2,4 +2,7 @@ import os
 import pytest
 
 dir_ = os.path.dirname(__file__)
-pytest.main(['{0}/tests/'.format(dir_), '--cov=nuketemplate', '--cov-report', 'term-missing'])
+pytest.main(['{0}/tests/'.format(dir_),
+             '--cov=nuketemplate',
+             '--cov-report',
+             'term-missing'])

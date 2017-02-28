c7a07e1006b25853f4af7cedfa730ec509293f25
Florian Einfalt <florian.einfalt@me.com>
2017-02-28 17:42:55 +0000

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,43 @@
 # nuketemplate py.test configuration
+import os
+import json
+import pytest
+
+from nuketemplate.template import AbstractTemplate
+
+
+@pytest.fixture(scope='session')
+def nuke():
+    import nuke
+    return nuke
+
+
+@pytest.fixture(scope='session')
+def api_():
+    with open('{0}/{1}/api.json'.format(os.path.dirname(__file__),
+                                        'test_template'), 'r') as fp:
+        return json.load(fp)
+
+
+@pytest.fixture(scope='session')
+def uuid_():
+    with open('{0}/{1}/uuid.json'.format(os.path.dirname(__file__),
+                                         'test_template'), 'r') as fp:
+        return json.load(fp)
+
+
+@pytest.fixture(scope='session')
+def template_data(api_, uuid_):
+    data = {'components': ['background', 'shadow', 'lights', 'windows'],
+            'options': ['base', 'grade', 'wheel'],
+            'api_data': api_['options'],
+            'uuid': uuid_}
+    return data
+
+
+@pytest.fixture(scope='session')
+def template(template_data):
+    template = AbstractTemplate(root='{0}/{1}'.format(
+                                os.path.dirname(__file__),
+                                'test_template'))
+    return template
--- tests/test_nuketemplate.py
+++ tests/test_nuketemplate.py
@@ -1,1 +1,17 @@
 # nuketemplate tests
+from nuketemplate.convert import AbstractTemplateConverter
+
+
+def test_template(template, template_data):
+    template.render(**template_data)
+    assert len(template.template) == 9
+    assert all([isinstance(temp, dict) for temp in template.template])
+
+
+def test_converter(template):
+    converter = AbstractTemplateConverter(template.template)
+    converter.convert()
+
+
+def test_builder(converter):
+    pass

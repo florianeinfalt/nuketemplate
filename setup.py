34f8690325e8223cd3f538b55424e35fbab97806
Florian Einfalt <florian.einfalt@me.com>
2017-02-28 12:14:59 +0000

    Require nukeuuid

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.rst', 'r') as f:
     readme = f.read()
 
-requirements = ['attrs', 'jinja2', 'networkx', 'nukecontexts']
+requirements = ['attrs', 'jinja2', 'networkx', 'nukecontexts', 'nukeuuid']
 test_requirements = ['pytest', 'pytest-cov',
                      'sphinx', 'sphinx_rtd_theme']
 
